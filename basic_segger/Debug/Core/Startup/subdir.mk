889470df013f5b7a3ea9d9702b4f703896deddf8
Pranjal Arya <pranjalarya9999@gmail.com>
2023-08-16 18:57:34 +0530

    Update SEGGER project (#4)
    Signed-off-by: Pranjal Arya <pranjalarya9999@gmail.com>

--- basic_segger/Debug/Core/Startup/subdir.mk
+++ basic_segger/Debug/Core/Startup/subdir.mk
@@ -16,7 +16,7 @@ S_DEPS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 Core/Startup/%.o: ../Core/Startup/%.s Core/Startup/subdir.mk
-	arm-none-eabi-gcc -mcpu=cortex-m4 -g3 -DDEBUG -c -I"C:/Users/pranj/Pranjal/Github/divy_frtos/trial_2/ThirdParty/SEGGER/Config" -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@" "$<"
+	arm-none-eabi-gcc -mcpu=cortex-m4 -g3 -DDEBUG -c -I"C:/Users/pranj/Pranjal/Github/test_frtos/basic_segger/ThirdParty/SEGGER/Config" -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@" "$<"
 
 clean: clean-Core-2f-Startup
 
